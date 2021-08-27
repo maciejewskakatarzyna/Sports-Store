b10d2b1aa17e3ae6ffaedffae6727122afd184e6
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-08-27 09:04:23 +0200

    add CartSummary

--- src/shop/Shop.js
+++ src/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {CategoryNavigation} from "./CategoryNavigation";
 import {ProductList} from "./ProductList";
+import {CartSummary} from "./CartSummary";
 
 export class Shop extends Component {
     render() {
@@ -11,6 +12,7 @@ export class Shop extends Component {
                         <div className="navbar-brand">
                             Sklep SPORTSSTORE
                         </div>
+                        <CartSummary {...this.props} />
                     </div>
                 </div>
                 <div className="row">
@@ -19,7 +21,8 @@ export class Shop extends Component {
                                             categories={this.props.categories}/>
                     </div>
                     <div className="col-9 p-2">
-                        <ProductList products={this.props.products}/>
+                        <ProductList products={this.props.products}
+                        addToCart={this.props.addToCart}/>
                     </div>
                 </div>
             </div>
